046afcbd852d7a39d1e37ec54bc5328c7b2ff048
Maxime Lerasle <maxlerasle@gmail.com>
2020-11-16 08:52:23 +0100

    Init project

---
+++ components/HomeCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img :src="home.images[0]" style="width:200px"><br/>
+    {{ home.title }}<br/>
+    {{ home.location.address }} {{ home.location.city }} {{ home.location.state }}<br>
+    {{ home.guests }} guests<br/>
+    {{ home.pricePerNight }} / night<br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float:left; margin:10px;">
+      <home-card :home="home" />
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes';
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
